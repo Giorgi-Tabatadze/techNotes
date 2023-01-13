7b70abd46f0eb9badda2eba2db10efe5210899dc
Giorgi-Tabatadze <giorgi.taba@icloud.com>
2023-01-13 17:15:27 +0400

    prepare for deployment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,5 @@
 const allowedOrigins = [
-  "http://localhost:3000", 
-  "https://www.dandrepairshop.com",
-  "https://dandrepairshop.com"
+  process.env.ALLOWED_ORIGINS
 ]
 
 module.exports = allowedOrigins
